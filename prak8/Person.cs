e28d04e83a26442f39729fab0f011b85e8c93366
Muhammad Yudha Maputra <yudhay@students.amikom.ac.id>
2023-06-14 13:55:51 +0700

    first commit

---
+++ prak8/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        // Properties
+        public string Name;
+        public int Age;
+        // Constructor
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public Person()
+        {
+
+        }
+        // Method
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun.", this.Name, this.Age);
+        }
+    }
+    // Child Class
+    public class Karyawan : Person
+    {
+        // Properties
+        public string KaryawanID;
+        public string Subject;
+
+        // Constructor
+        public Karyawan(string KaryawanID, string Subject)
+        {
+            this.KaryawanID = KaryawanID;
+            this.Subject = Subject;
+        }
+        public Karyawan()
+        {
+
+        }
+
+        // Override Method
+        public void InfoPerson(string Subject, string KaryawanID)
+        {
+            this.Subject = Subject;
+            this.KaryawanID = KaryawanID;
+            Console.WriteLine("Saya disini sebagai {0} dengan ID Karyawan {1}.", this.Subject, this.KaryawanID);
+        }
+    }
+}
---
+++ prak8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person1 = new Person();
+            Person1.Name = "Muhammad Yudha M";
+            Person1.Age = 19;
+            Person1.InfoPerson();
+
+            Karyawan Karyawan1 = new Karyawan();
+            Karyawan1.Name = "Gillas";
+            Karyawan1.Age = 19;
+            Karyawan1.InfoPerson();
+            Karyawan1.InfoPerson("Dosen", "6026200404040001");
+
+            Console.ReadLine();
+        }
+    }
+}
